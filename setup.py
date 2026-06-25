72a91c25585e8fb6ef61579c446c929d1a6422fe
Aman Singh <amankrsingh764@gmail.com>
2026-06-25 18:19:46 +0530

    chore: add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="Aman Singh",
+    author_email="aman.singh@example.com",
+    packages=find_packages(),
+    install_requires=[]
+)
